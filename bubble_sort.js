b744bbec421e5ddabb7fd4b12ffa3f9ef805138e
Pedro Machado <pedroeugenio.dev@gmail.com>
2024-11-25 15:34:09 -0300

    Bubble sort with stop flag

--- bubble_sort.js
+++ bubble_sort.js
@@ -3,13 +3,17 @@ const arr = [5,2,8,1,3]
 function bubble_sort(arr) {
     let aux
     for (let i = 0; i < arr.length; i++) {
+        let swapped = false
         for (let j = 0; j < (arr.length - 1); j++) {
+            console.log(arr)
             if (arr[j] > arr[j+1]){
                 aux = arr[j]
                 arr[j] = arr[j+1]
                 arr[j+1] = aux
+                swapped = true;
             }
         }
+        if(swapped === false) break
     }
 }
 
