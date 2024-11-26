d43b337fd5d31fbb2edbd7c5dec89fe2443b8684
Pedro Machado <pedroeugenio.dev@gmail.com>
2024-11-26 12:32:25 -0300

    Bubble sort with stop flag and reducted comparison

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,19 +1,20 @@
-const arr = [5,2,8,1,3]
+const arr = [1,2,4,3,5]
 
 function bubble_sort(arr) {
     let aux
     for (let i = 0; i < arr.length; i++) {
         let swapped = false
-        for (let j = 0; j < (arr.length - 1); j++) {
-            console.log(arr)
+        for (let j = 0; j < (arr.length - (i+1)); j++) {
             if (arr[j] > arr[j+1]){
                 aux = arr[j]
                 arr[j] = arr[j+1]
                 arr[j+1] = aux
-                swapped = true;
+                swapped = true
             }
         }
-        if(swapped === false) break
+        if(!swapped){
+            break;
+        }
     }
 }
 
